5a095ab959d3651a19bab4fac79eefc2ccd9c5c1
Abdoul Akano <akano1nassirou@gmail.com>
2023-07-24 09:52:49 -0400

    all data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 import './App.css'
 import ContactList from './components/ContactList'
 
+
+
 function App() {
   const [count, setCount] = useState(0)
+  const [selectedContactId, setSelectedContactId] = useState(null);
 
   return (
     <>
-      <ContactList />
+       {selectedContactId ? (
+        <div>Selected Contact View</div>
+      ) : (
+        <ContactList />
+      )}
     </>
   )
 }
